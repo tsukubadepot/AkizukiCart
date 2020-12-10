6ecababc6349a8ef846c6c27e149e61db748def3
tsukubadepot <tsukubadepot@icloud.com>
2020-12-10 20:38:18 +0900

    Initial Commit

---
+++ AkizukiCart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AkizukiCart
+//
+//  Created by Jun Yamashita on 2020/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
