f502cdccdc816d1fbff56723266e2d7723b4958c
Jun Yamashita <jun.yamashita@tsukubadepot.net>
2021-02-09 22:33:30 +0900

    正規表現適用後の値の返し方が間違っていたので修正。
    当初、範囲内の適用される全ての正規表現を返すつもりで記述していた部分が間違っていたので、検索対象内にマッチする全ての正規表現の配列を返すように修正した。

--- AkizukiCart/Extensions/StringExtension.swift
+++ AkizukiCart/Extensions/StringExtension.swift
@@ -9,16 +9,16 @@ import Foundation
 
 extension String {
     // https://zenn.dev/kyome/articles/1a55547614dd495a869d
+    // http://iwakwak.hatenablog.com/entry/2019/11/13/191142
     // オリジナルはマッチしない、あるいは error の場合には [] を返していたが、nil に変更
     /// 正規表現にマッチした文字列を得る
     /// - Parameter pattern: 正規表現
     /// - Returns: マッチした文字列の配列。マッチしなかった場合には nil
     func match(_ pattern: String) -> [String]? {
-        guard let regex = try? NSRegularExpression(pattern: pattern),
-              let matched = regex.firstMatch(in: self, range: NSRange(location: 0, length: self.count))
-        else { return nil }
-        return (0 ..< matched.numberOfRanges).map {
-            NSString(string: self).substring(with: matched.range(at: $0))
+        guard let regex = try? NSRegularExpression(pattern: pattern) else {
+            return nil
         }
+        
+        return regex.matches(in: self, range: NSRange(location: 0, length: self.count)).map { String(self[Range($0.range, in:self)!]) }
     }
 }
