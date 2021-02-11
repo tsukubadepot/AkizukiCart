28214459d2eb98475f4ac5c0b15a7507e9eab63e
Jun Yamashita <jun.yamashita@tsukubadepot.net>
2021-02-11 11:56:42 +0900

    複数商品を検索させるための拡張
    複数商品を検索させるために必要となる新しい型と、それに対応したメソッドの追加。
    APIの仕様上、不明な点があるので、それについては今後修正する必要がある。

--- AkizukiCart/API Class/APIHandler.swift
+++ AkizukiCart/API Class/APIHandler.swift
@@ -73,4 +73,79 @@ class APIHandler {
             }
         }
     }
+    
+    /// 指定されたアイテム番号で商品を検索する
+    /// - Parameters:
+    ///   - items: ^[MKPBRSICT]-[0-9]{5}$.json 形式のアイテム番号で構成された商品名の配列
+    ///   - successHandler: 検索成功時の処理ハンドラ
+    ///   - notfoundHandler: 該当商品が見つからない場合の処理ハンドラ
+    ///   - errorHadler: 例外処理（ネットワークエラーなど）の処理ハンドラ
+    static func searchItems(_ items: [String], successHandler: @escaping (Components) -> Void, notfoundHandler: @escaping (FailureResult) -> Void, errorHadler: @escaping (Error) -> Void) {
+        // FIXME: 現状だと、items 内に不正なIDがあったとしても、respose の components 内に　2つの型が混在して返還される。
+        // 当面の対策として、成功時とエラー時に戻されるキー両方を盛り込んだ Struct を作り、それで強引に一致させて返す
+        // TODO: 根本的には、components キーに含まれる型に合わせて処理を切り分ける必要があるが、decoder でうまく切り分けが進まないので、今後の課題とする
+        // 大枠として components を取得し、その中で振り分けながら処理させる必要があるかもしれない。
+        // - compoments 内の全ての要素に対して
+        // -- 200 だったら PartsInfo を取得して、対応する配列に入れる
+        // -- それ以外であれば、エラー用の配列に入れる
+        
+        let searchURL = URL(string: "https://akizuki-api.appspot.com/component/list.json")!
+        
+        let parameters = ["ids": items]
+
+        AF.request(searchURL,
+                   method: .post,
+                   parameters: parameters,
+                   encoding: JSONEncoding.default)
+            .responseJSON { result in
+                if let error = result.error {
+                    DispatchQueue.main.async {
+                        errorHadler(error)
+                        
+                        return
+                    }
+                }
+                
+                switch result.result {
+                case .failure(let error):
+                    // タイムアウトなど
+                    DispatchQueue.main.async {
+                        errorHadler(error)
+                        return
+                    }
+                    
+                case .success:
+                    print("Code:", result.response!.statusCode)
+                    switch result.response!.statusCode {
+                    case 200...299:
+                        do {
+                            let components = try JSONDecoder().decode(Components.self, from: result.data!)
+                                                        
+                            DispatchQueue.main.async {
+                                successHandler(components)
+                            }
+                        } catch {
+                            DispatchQueue.main.async {
+                                errorHadler(error)
+                            }
+                        }
+                        
+                    case 400...499:
+                        do {
+                            let fail = try JSONDecoder().decode(FailureResult.self, from: result.data!)
+                            DispatchQueue.main.async {
+                                notfoundHandler(fail)
+                            }
+                        } catch {
+                            DispatchQueue.main.async {
+                                errorHadler(error)
+                            }
+                        }
+                        
+                    default:
+                        fatalError()
+                    }
+                }
+            }
+    }
 }
